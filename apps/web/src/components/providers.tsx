dd6a64639cb1640d6cf8a5a0824277f99a35aaab
Andrey Reznik <30355042+andrey-reznik@users.noreply.github.com>
2026-02-05 17:36:31 +0300

    feat: Auth

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,14 +1,18 @@
 'use client'
 
+import { AuthUIProvider } from '@daveyplate/better-auth-ui'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { authClient } from '@/lib/auth-client'
 import { queryClient } from '@/utils/orpc'
-
 import { ThemeProvider } from './theme-provider'
 import { Toaster } from './ui/sonner'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
   return (
     <ThemeProvider
       attribute="class"
@@ -17,7 +21,18 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       enableSystem
     >
       <QueryClientProvider client={queryClient}>
-        {children}
+        <AuthUIProvider
+          authClient={authClient}
+          Link={Link}
+          navigate={router.push}
+          onSessionChange={() => {
+            // Clear router cache (protected routes)
+            router.refresh()
+          }}
+          replace={router.replace}
+        >
+          {children}
+        </AuthUIProvider>
         <ReactQueryDevtools />
       </QueryClientProvider>
       <Toaster richColors />

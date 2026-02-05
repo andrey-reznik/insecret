5187578817f2082a871a5324f2bb574181a63551
Andrey Reznik <30355042+andrey-reznik@users.noreply.github.com>
2026-02-05 15:51:37 +0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import prisma from "@insecret/db";
+import { env } from "@insecret/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "sqlite",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
